ea45cabef8952570fce159f0b7eb1a865310a747
timg777 <timg777@yandex.ru>
2025-12-03 16:05:11 +0500

    [Refactor] NFTService - renamed - Sendable safety - moved up to Services folder

---
+++ iOS-FakeNFT-Extended/Services/NFTService.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+protocol NFTServiceProtocol: Sendable {
+    func loadNft(id: String) async throws -> NFT
+}
+
+actor NFTService: NFTServiceProtocol {
+
+	private let api: ObservedNetworkClient
+	private let storage: NFTStorageProtocol
+
+	init(api: ObservedNetworkClient, storage: NFTStorageProtocol) {
+        self.storage = storage
+        self.api = api
+    }
+
+    func loadNft(id: String) async throws -> NFT {
+        if let nft = await storage.getNft(with: id) {
+            return nft
+        }
+
+		let request = GetNFTByIDRequest(id: id)
+		let nft = try await api.getNFT(by: id)
+        await storage.saveNft(nft)
+        return nft
+    }
+}
