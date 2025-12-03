bfb9d670e77f1361de3e327196b47f6a3f61389e
timg777 <timg777@yandex.ru>
2025-12-03 15:55:36 +0500

    [Feature] DataLoader - added to track loading state during any network request

---
+++ iOS-FakeNFT-Extended/Foundation/NetworkClient/DataLoader.swift
@@ -0,0 +1,41 @@
+//
+//  DataLoader.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Superior Warden on 03.12.2025.
+//
+
+import Observation
+
+enum LoadingState: Equatable {
+	case idle
+	case fetching
+	case error
+}
+
+@MainActor
+@Observable
+final class DataLoader {
+	private(set) var loadingState: LoadingState = .idle
+	
+	func fetchData<T: Decodable>(
+		_ operation: @escaping @Sendable () async throws -> T
+	) async throws -> T {
+		loadingState = .fetching
+		
+		do {
+			defer {
+				resetState()
+			}
+			return try await operation()
+		} catch {
+			loadingState = .error
+			print("[DataLoader] fetch error: \(error.localizedDescription)")
+			throw error
+		}
+	}
+	
+	func resetState() {
+		loadingState = .idle
+	}
+}
