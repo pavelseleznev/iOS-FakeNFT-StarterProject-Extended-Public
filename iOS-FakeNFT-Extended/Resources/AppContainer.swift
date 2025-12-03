67d93f6ac59215392f35e4057fa6ccd841254e07
timg777 <timg777@yandex.ru>
2025-12-03 16:06:32 +0500

    [Feature] Resources - assets added (Assets + Images + Fonts) - Page, Sheet added

---
+++ iOS-FakeNFT-Extended/Resources/AppContainer.swift
@@ -0,0 +1,12 @@
+//
+//  AppContainer.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Superior Warden on 03.12.2025.
+//
+
+
+struct AppContainer {
+	let nft: NFTServiceProtocol
+	let api: ObservedNetworkClient
+}
---
+++ iOS-FakeNFT-Extended/Resources/Images.swift
@@ -0,0 +1,22 @@
+//
+//  Images.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Superior Warden on 02.12.2025.
+//
+
+import SwiftUI
+
+extension Image {
+	static let edit = Image(systemName: "square.and.pencil")
+	static let xmark = Image(systemName: "xmark")
+	static let sort = Image(.sort)
+	static let chevronLeft = Image(systemName: "chevron.left")
+	static let chevronRight = Image(systemName: "chevron.right")
+	static let addToCart = Image(.addToCart)
+	static let removeFromCart = Image(.removeFromCart)
+	static let heartFill = Image(systemName: "heart.fill")
+	static let plusCircle = Image(systemName: "plus.circle.fill")
+	static let checkmarkCircle = Image(systemName: "checkmark.circle.fill")
+	static let starFill = Image(systemName: "star.fill")
+}
