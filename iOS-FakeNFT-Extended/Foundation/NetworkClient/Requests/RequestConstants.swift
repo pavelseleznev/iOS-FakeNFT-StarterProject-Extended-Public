4b170328c17b7b695aab33436d69dae4453fb95b
timg777 <timg777@yandex.ru>
2025-12-06 22:32:29 +0500

    [Feature] RequestConstants - token added

--- iOS-FakeNFT-Extended/Foundation/NetworkClient/Requests/RequestConstants.swift
+++ iOS-FakeNFT-Extended/Foundation/NetworkClient/Requests/RequestConstants.swift
@@ -1,5 +1,4 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-#warning("insert your token here")
-    static let token = ""
+    static let token = "b55ff78d-18d9-4818-a341-4bc6d596072a"
 }
