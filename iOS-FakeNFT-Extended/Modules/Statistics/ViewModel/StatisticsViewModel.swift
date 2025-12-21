f6d2bc01d4f068e4dd2f0f543269a323c6c64528
timg777 <timg777@yandex.ru>
2025-12-21 18:35:40 +0500

    [Refactor] StatisticsViewModel - real data usage instead of mocks

--- iOS-FakeNFT-Extended/Modules/Statistics/ViewModel/StatisticsViewModel.swift
+++ iOS-FakeNFT-Extended/Modules/Statistics/ViewModel/StatisticsViewModel.swift
@@ -5,23 +5,18 @@
 //  Created by Superior Warden on 08.12.2025.
 //
 
+import Foundation
 import Observation
 
 @MainActor
 @Observable
 final class StatisticsViewModel {
-	private(set) var users: [UserListItemResponse] = [
-		.mock,
-		.mock,
-		.mock,
-		.mock,
-		.mock,
-		.mock,
-		.mock,
-		.mock
-	]
+	private(set) var users = Set<UserListItemResponse>()
 	private let api: ObservedNetworkClient
 	private let push: (Page) -> Void
+	private var currentPage = 0
+	
+	var currenctSortOption: StatisticsSortActionsViewModifier.SortOption = .name
 	
 	init(
 		api: ObservedNetworkClient,
@@ -30,15 +25,42 @@ final class StatisticsViewModel {
 		self.api = api
 		self.push = push
 	}
-	
-	func viewDidAppear() {}
 }
 
 extension StatisticsViewModel {
-	func applySortByName() {}
-	func applySortByRate() {}
+	@inline(__always)
+	var visibleUsers: [UserListItemResponse] {
+		users
+			.sorted(by: usersSortComparator)
+	}
 	
 	func didTapUserCell(for user: UserListItemResponse) {
 		push(.statProfile(profile: user))
+	}
+	
+	@Sendable
+	func loadNextUsersPage(onAppear: Bool = false) async {
+		guard (onAppear && currentPage == 0) || !onAppear else { return }
+		do {
+			try await api
+				.getUsers(page: currentPage, sortOption: currenctSortOption)
+				.forEach {
+					users.insert($0)
+				}
+			currentPage += 1
+		} catch {
+			print(error)
+		}
+	}
+}
+
+private extension StatisticsViewModel {
+	func usersSortComparator(_ first: UserListItemResponse, _ second: UserListItemResponse) -> Bool {
+		switch currenctSortOption {
+		case .rate:
+			first.rating.localizedStandardCompare(second.rating) == .orderedAscending
+		case .name:
+			first.name.localizedStandardCompare(second.name) == .orderedAscending
+		}
 	}
 }
