10bd63c3684d6ebcda4113f940c211ad4e6d51b1
timg777 <timg777@yandex.ru>
2025-12-25 23:18:28 +0500

    [Feature] SplashView - has error alert now - loads user data - calls onComplete callback on finish loading to perform main flow

--- iOS-FakeNFT-Extended/Modules/Splash/SplashView.swift
+++ iOS-FakeNFT-Extended/Modules/Splash/SplashView.swift
@@ -8,16 +8,70 @@
 import SwiftUI
 
 struct SplashView: View {
+	@State private var dataLoadingErrorIsPresented = false
+	
+	let appContainer: AppContainer
+	let onComplete: () -> Void
+	private let animation: Animation = .easeInOut(duration: 0.15)
+
 	var body: some View {
 		ZStack {
 			Color.ypWhite.ignoresSafeArea()
+			
+			let state = appContainer.api.loadingState
 			Image(.vector)
+				.scaleEffect(state == .idle ? 1 : 0)
+				.opacity(state == .idle ? 1 : 0.1)
+				.overlay {
+					ProgressView()
+						.scaleEffect(state != .idle ? 1.5 : 0)
+						.progressViewStyle(.circular)
+				}
+		}
+		.animation(animation, value: appContainer.api.loadingState)
+		.task(priority: .high) {
+			await loadUserData()
+		}
+		.applyRepeatableAlert(
+			isPresneted: $dataLoadingErrorIsPresented,
+			message: .cantGetProfileData,
+			didTapRepeat: {
+				Task(priority: .high) { await loadUserData() }
+			}
+		)
+		.transition(.opacity)
+	}
+	
+	private func loadUserData() async {
+		do {
+			let profile = try await appContainer.api.getProfile()
+			let cart = try await appContainer.api.getOrder()
+			
+			await appContainer.nftService
+				.didLoadUserData(
+					likes: profile.likes,
+					purchased: profile.nfts,
+					cart: cart.nftsIDs
+				)
+			
+			onComplete()
+		} catch {
+			guard !(error is CancellationError) else { return }
+			withAnimation(animation) {
+				dataLoadingErrorIsPresented = true
+			}
 		}
 	}
 }
 
 #if DEBUG
 #Preview {
-	SplashView()
+	SplashView(
+		appContainer: .init(
+			nftService: NFTService(api: .mock, storage: NFTStorage()),
+			api: .mock
+		),
+		onComplete: {}
+	)
 }
 #endif
