1f7a798ac78d9f51cf0a9fc5bb64b7cd05cde44d
timg777 <timg777@yandex.ru>
2025-12-25 22:46:28 +0500

    [Feature] CurrencyResponse - mock added - Equatable now - CurrencyContainer added

--- iOS-FakeNFT-Extended/Models/Network/CurrencyResponse.swift
+++ iOS-FakeNFT-Extended/Models/Network/CurrencyResponse.swift
@@ -5,9 +5,25 @@
 //  Created by Superior Warden on 05.12.2025.
 //
 
-struct CurrencyResponse: Decodable {
+import Foundation
+
+struct CurrencyResponse: Decodable, Equatable {
 	let title: String
 	let name: String
 	let image: String
+	let id: String
+	
+	static var mock: Self {
+		.init(
+			title: "Bitcoin",
+			name: "BTC",
+			image: "",
+			id: UUID().uuidString
+		)
+	}
+}
+
+struct CurrencyContainer: Identifiable, Equatable {
+	let currency: CurrencyResponse
 	let id: String
 }
