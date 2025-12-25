acac5cff54a23a9293d260c6deac7419a99aa7ba
timg777 <timg777@yandex.ru>
2025-12-25 22:59:59 +0500

    [Feature] Tab - localized - cart added

--- iOS-FakeNFT-Extended/Modules/TabView/ViewModel/Tab.swift
+++ iOS-FakeNFT-Extended/Modules/TabView/ViewModel/Tab.swift
@@ -10,16 +10,19 @@ import SwiftUI
 enum Tab: String, CaseIterable, Identifiable {
 	case profile
 	case catalog
+	case cart
 	case statistics
 	
-	var title: String {
+	var title: LocalizedStringResource {
 		switch self {
 		case .profile:
-			"Профиль"
+			.profile
 		case .catalog:
-			"Каталог"
+			.catalog
+		case .cart:
+			.cart
 		case .statistics:
-			"Статистика"
+			.statistics
 		}
 	}
 	
@@ -30,6 +33,8 @@ enum Tab: String, CaseIterable, Identifiable {
 			Image.profilePerson
 		case .catalog:
 			Image.catalog
+		case .cart:
+			Image.shoppingBag
 		case .statistics:
 			Image.statistics
 		}
@@ -49,11 +54,19 @@ enum Tab: String, CaseIterable, Identifiable {
 				appContainer: appContainer,
 				push: push
 			)
+			
 		case .catalog:
 			CatalogView(
 				appContainer: appContainer,
 				push: push
 			)
+			
+		case .cart:
+			CartView(
+				nftService: appContainer.nftService,
+				push: push
+			)
+			
 		case .statistics:
 			StatisticsView(
 				api: appContainer.api,
