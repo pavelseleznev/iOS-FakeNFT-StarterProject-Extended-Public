d8dee65c401c0248ab402bc2b022efe16499f796
timg777 <timg777@yandex.ru>
2025-12-25 23:05:42 +0500

    [Feature] NFTNameRateAuthorView - skeleton support added

--- iOS-FakeNFT-Extended/Modules/UIComponents/NFT/UIComponents/NFTNameRateAuthorView.swift
+++ iOS-FakeNFT-Extended/Modules/UIComponents/NFT/UIComponents/NFTNameRateAuthorView.swift
@@ -8,22 +8,33 @@
 import SwiftUI
 
 struct NFTNameRateAuthorView: View {
-	
-	let model: NFTResponse
+	let model: NFTResponse?
 	let layout: NFTCellLayout
 	
 	var body: some View {
 		VStack(alignment: .leading, spacing: 6) {
-			Text(model.name)
+			Text(model?.name ?? "Jhon Doe")
 				.foregroundStyle(.ypBlack)
 				.font(.bold17)
+				.opacity(model == nil ? 0 : 1)
+				.overlay {
+					if model == nil {
+						LoadingShimmerPlaceholderView()
+					}
+				}
 			
-			RatingPreview(rating: model.rating)
+			RatingPreview(rating: model?.rating)
 			
 			if case .my = layout {
-				Text("от " + model.authorName)
+				Text("от " + (model?.authorName ?? "Jhon Doe"))
 					.foregroundStyle(.ypBlack)
 					.font(.regular13)
+					.opacity(model == nil ? 0 : 1)
+					.overlay {
+						if model == nil {
+							LoadingShimmerPlaceholderView()
+						}
+					}
 			}
 		}
 	}
