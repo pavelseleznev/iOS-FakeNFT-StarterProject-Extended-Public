b4c11690afbb1d16f2dfc87db8ea80a42715b03b
timg777 <timg777@yandex.ru>
2025-12-26 23:25:27 +0500

    [Feature] Skeleton - implemented

---
+++ iOS-FakeNFT-Extended/Modules/UIComponents/Skeleton.swift
@@ -0,0 +1,22 @@
+//
+//  Skeleton.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Superior Warden on 26.12.2025.
+//
+
+import SwiftUI
+
+extension View {
+	func applySkeleton<T>(_ data: T?) -> some View {
+		self
+			.opacity(data == nil ? 0 : 1)
+			.overlay {
+				if data == nil {
+					LoadingShimmerPlaceholderView()
+						.transition(.opacity)
+						.transition(.scale)
+				}
+			}
+	}
+}
