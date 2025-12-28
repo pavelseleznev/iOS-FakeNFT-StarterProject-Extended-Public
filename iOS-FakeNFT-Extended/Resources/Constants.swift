d940f41a52e1d4b868c6a0cf2a97cdf24f5e1c0d
timg777 <timg777@yandex.ru>
2025-12-28 23:42:31 +0500

    [Feature] Constants added

---
+++ iOS-FakeNFT-Extended/Resources/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Superior Warden on 27.12.2025.
+//
+
+import SwiftUI
+
+enum Constants {
+	static let isOnboardingCompleteKey = "isOnboardingCompleteKey"
+	static let appLaunchCountKey = "appLaunchCountKey"
+	static let ratingIsAlreadyPresentedThisLaunchKey = "ratingIsAlreadyPresentedThisLaunchKey"
+	
+	static let splashInfiniteCarouselSpeedRatio: CGFloat = 0.05
+	static let safeAreaHorizontalPadding: CGFloat = 15
+	static let onboardingCellSize: CGFloat = 100
+	static let tabIndicatorHeight: CGFloat = 4
+	
+	static let nftNameLineLimit = 2
+	static let authorNameLineLimit = 2
+	
+	static let defaultAnimation: Animation = .easeInOut(duration: 0.15)
+	
+	static let maxRatingStars = 5
+}
