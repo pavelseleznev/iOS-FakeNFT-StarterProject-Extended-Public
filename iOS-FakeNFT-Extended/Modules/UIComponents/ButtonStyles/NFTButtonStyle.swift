6bc261e19eb05ebc34ad96cd1a732944c159bc00
timg777 <timg777@yandex.ru>
2026-01-02 04:50:27 +0500

    [Feature] NFTButtonStyle - appearance feature

--- iOS-FakeNFT-Extended/Modules/UIComponents/ButtonStyles/NFTButtonStyle.swift
+++ iOS-FakeNFT-Extended/Modules/UIComponents/ButtonStyles/NFTButtonStyle.swift
@@ -19,6 +19,20 @@ private struct NFTButtonStyle: ButtonStyle {
 				RoundedRectangle(cornerRadius: 16)
 					.fill(filled ? Color.ypBlack : Color.clear)
 					.stroke(Color.ypBlack, lineWidth: 1)
+					.stroke(
+						LinearGradient(
+							stops: [
+								.init(color: .teal, location: 0),
+								.init(color: .clear, location: 0.3),
+								.init(color: .clear, location: 0.5),
+								.init(color: .clear, location: 0.7),
+								.init(color: .purple, location: 1),
+							],
+							startPoint: .topLeading,
+							endPoint: .bottomTrailing
+						),
+						lineWidth: filled ? 1 : 0.3
+					)
 			)
 			.opacity(configuration.isPressed ? 0.5 : 1)
 			.padding(.top, 16)
