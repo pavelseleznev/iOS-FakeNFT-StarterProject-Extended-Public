d1df2ad61c4e892baec258bf022623416222ba95
timg777 <timg777@yandex.ru>
2026-01-02 04:36:48 +0500

    [Feature] NetworkMonitor implementation with usage - implemented - preload in App - DataLoader usage

--- iOS-FakeNFT-Extended/App/iOS_FakeNFT_ExtendedApp.swift
+++ iOS-FakeNFT-Extended/App/iOS_FakeNFT_ExtendedApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct iOS_FakeNFT_ExtendedApp: App {
+	init() { let _ = NetworkMonitor.shared }
     var body: some Scene {
         WindowGroup {
 			CoordinatorView()
--- iOS-FakeNFT-Extended/Foundation/NetworkClient/DataLoader.swift
+++ iOS-FakeNFT-Extended/Foundation/NetworkClient/DataLoader.swift
@@ -6,6 +6,7 @@
 //
 
 import Observation
+import Foundation
 
 enum LoadingState: Equatable {
 	case idle
@@ -18,10 +19,15 @@ enum LoadingState: Equatable {
 final class DataLoader {
 	private(set) var loadingState: LoadingState = .idle
 	private let cancelErrorsDscr = ["отменено", "cancelled"]
+	private let monitor = NetworkMonitor.shared
 	
 	func fetchData<T: Decodable>(
 		_ operation: @escaping @Sendable () async throws -> T
 	) async throws -> T {
+		guard monitor.isOnline else {
+			throw NSError(domain: "No internet connection", code: 1)
+		}
+		
 		loadingState = .fetching
 		
 		do {
