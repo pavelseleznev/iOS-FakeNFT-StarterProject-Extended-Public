32d58413a97641a872582d649a9ea44a2f7665c1
timg777 <timg777@yandex.ru>
2026-01-09 02:10:34 +0500

    [Refactor] EditProfileForm - opmimized & DRY acception - forms redesign

--- iOS-FakeNFT-Extended/Modules/UIComponents/Profile/Components/EditProfileForm.swift
+++ iOS-FakeNFT-Extended/Modules/UIComponents/Profile/Components/EditProfileForm.swift
@@ -7,46 +7,78 @@
 
 import SwiftUI
 
+#warning("TODO: Loalize all fields")
 struct EditProfileForm: View {
     @Binding var name: String
     @Binding var about: String
     @Binding var website: String
-    @FocusState var focusedField: EditProfileField?
 
     var body: some View {
         VStack(spacing: 24) {
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Имя").font(.bold22)
-                TextField("", text: $name)
-                    .padding()
-                    .font(.regular17)
-                    .background(Color.ypLightGrey)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .focused($focusedField, equals: .name)
-            }
-            .padding(.horizontal, 16)
+			FormView(title: "Имя", placeholder: "Написать...", text: $name)
 
             VStack(alignment: .leading, spacing: 8) {
-                Text("Описание").font(.bold22)
-                AutoSizingTextEditor(text: $about, minHeight: 100, font: .regular17)
-                    .padding(8)
-                    .background(Color.ypLightGrey)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .scrollContentBackground(.hidden)
-                    .focused($focusedField, equals: .about)
+                Text("Описание")
+					.font(.bold22)
+					.padding(.leading)
+				
+				AutoSizingTextEditor(
+					text: $about,
+					placeholder: "Написать..."
+				)
             }
-            .padding(.horizontal, 20)
-
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Сайт").font(.bold22)
-                TextField("", text: $website)
-                    .padding()
-                    .font(.regular17)
-                    .background(Color.ypLightGrey)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .focused($focusedField, equals: .website)
-            }
-            .padding(.horizontal, 16)
+			
+			FormView(
+				title: "Сайт",
+				placeholder: "Написать...",
+				text: $website
+			)
         }
+		.padding(.horizontal, 16)
     }
+}
+
+// MARK: - Helper
+fileprivate struct FormView: View {
+	let title: String
+	let placeholder: String
+	@Binding var text: String
+	
+	var body: some View {
+		VStack(alignment: .leading, spacing: 8) {
+			Text(title)
+				.font(.bold22)
+				.padding(.leading)
+			
+			TextField(placeholder, text: $text)
+				.padding()
+				.font(.regular17)
+				.background(Color.ypLightGrey)
+				.clipShape(.capsule)
+				.shadow(color: .ypBlackUniversal.opacity(0.2), radius: 10)
+		}
+	}
+}
+
+// MARK: - Preview
+#Preview("Full content") {
+	Color.ypWhite.ignoresSafeArea()
+		.overlay {
+			EditProfileForm(
+				name: .constant("GAGAGAGAGA"),
+				about: .constant("Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100 NFT, и еще больше — на моём сайте…Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100 NFT, и еще больше — на моём сайте…Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100 NFT, и еще больше — на моём сайте…Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100 NFT, и еще больше — на моём сайте…"),
+				website: .constant("GAGAGAGAG")
+			)
+		}
+}
+
+#Preview("Empty content") {
+	Color.ypWhite.ignoresSafeArea()
+		.overlay {
+			EditProfileForm(
+				name: .constant(""),
+				about: .constant(""),
+				website: .constant("")
+			)
+		}
 }
